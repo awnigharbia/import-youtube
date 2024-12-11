509eebd0eff4d157b2e74943b62d1d805deb0666
Awni Gharbia <anongharbia@gmail.com>
2024-12-11 12:09:51 +0200

    add new package

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const { downloadVideo } = require('yt-dlp-video');
-
-// Download video in best quality MP4 format
-async function download() {
-    await downloadVideo('https://www.youtube.com/watch?v=IJHrPjx4egM');
-}
-
-download();
