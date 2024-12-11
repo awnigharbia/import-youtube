9c820e367e5d078fe37646cc71e0f5c006836e55
Awni Gharbia <anongharbia@gmail.com>
2024-12-11 13:17:09 +0200

    test

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 import ytdl from 'youtube-dl-exec';
 
 async function download() {
-    const data = ytdl('https://www.youtube.com/watch?v=IJHrPjx4egM', {
+    const data = await ytdl('https://www.youtube.com/watch?v=IJHrPjx4egM', {
         dumpSingleJson: true,
         noWarnings: true,
         cookies: 'cookie.txt'
