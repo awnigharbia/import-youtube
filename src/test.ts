e3598a30211c185e0b413985950c43d95cee21df
Awni Gharbia <anongharbia@gmail.com>
2024-12-11 12:11:01 +0200

    fixpath

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@ import youtubeDl from "youtube-dl-exec";
 async function download() {
     const res = await youtubeDl('https://youtu.be/IJHrPjx4egM', {
         dumpSingleJson: true,
-        cookies: 'cookies.txt',
+        cookies: './cookies.txt',
     })
 
     console.log(res);
